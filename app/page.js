5ee68bf6bf54084c6e4d08dc664665e24053d06f
yuvrajsinghnegi012 <144053547+yuvrajsinghnegi012@users.noreply.github.com>
2025-03-26 16:27:23 +0530

    completed header component  UI.

--- app/page.js
+++ app/page.js
@@ -1,10 +1,6 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div>
-      <h1>Welcome To Next.js</h1><br />
-      <Button variant="destructive" className="cursor-pointer">Click Here for Popup</Button>
     </div>
   )
 }
--- components/header.jsx
+++ components/header.jsx
@@ -1,13 +1,83 @@
 import React from 'react';
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
+import Image from 'next/image';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import { ArrowLeft, CarFront, Layout } from 'lucide-react';
+import { Button } from './ui/button';
 
-const Header = () => {
+const Header = ({ isAdminPage = false }) => {
+  const isAdmin = false;
+
   return (
-    <header className="px-4 mx-auto text-center bg-orange-500">
-      <h1>Dream Car Showcase</h1>
-      <SignedIn>
-        <UserButton className="cursor-pointer"/>
-      </SignedIn>
+    <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href={isAdminPage ? "/admin" : "/"} className="flex">
+          <Image
+            src={"/logo.png"}
+            alt="Vehiql Logo"
+            width={200}
+            height={60}
+            className="h-12 2-auto object-contain"
+          />
+          {
+            isAdminPage && (
+              <span className="text-xs font-extralight">admin</span>
+            )
+          }
+        </Link>
+
+        <div className="flex items-center space-x-4">
+          {isAdminPage ? (
+            <Link href="/" className="cursor-pointer">
+              <Button varient="outline" className="flex items-center gap-2">
+                <ArrowLeft size={18} />
+                <span>Back To App</span>
+              </Button>
+            </Link>
+          ) : (
+            <SignedIn>
+              <Link href="/saved-cars">
+                <Button className="cursor-pointer">
+                  <CarFront size={18} />
+                  <span className="hidden md:inline">Saved Cars</span>
+                </Button>
+              </Link>
+              {
+                !isAdmin ? (<Link href="/reservations">
+                  <Button variant="outline" className="cursor-pointer">
+                    <CarFront size={18} />
+                    <span className="hidden md:inline">My Reservations</span>
+                  </Button>
+                </Link>
+                ) : (
+                  <Link href="/admin">
+                    <Button variant="outline" className="cursor-pointer">
+                      <Layout size={18} />
+                      <span className="hidden md:inline">Admin Portal</span>
+                    </Button>
+                  </Link>
+                )
+              }
+            </SignedIn>
+          )}
+
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/">
+              <Button varient="outline" className="cursor-pointer">Login</Button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10"
+                }
+              }}
+            />
+          </SignedIn>
+        </div>
+      </nav>
     </header>
   )
 }
